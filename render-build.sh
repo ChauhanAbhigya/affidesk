0b95012f7ddcba6d14d1105c46ef49f24b9a08f9
hitesh <chauhanabhigya0@gmail.com>
2025-09-16 09:54:03 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-apt-get update && apt-get install -y \
-    wkhtmltopdf \
-    xfonts-75dpi \
-    xfonts-base \
-    fonts-noto \
-    fonts-noto-cjk \
-    fonts-noto-color-emoji \
-    fonts-deva
-
+#!/usr/bin/env bash
+apt-get update && apt-get install -y wkhtmltopdf
 pip install -r requirements.txt
